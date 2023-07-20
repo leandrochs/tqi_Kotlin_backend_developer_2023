605a1dd4c80182f263634657f3f0cb13946a67c3
leandrochs <leandrochagask@gmail.com>
2023-07-19 23:47:11 -0300

    feat: adiciona crud para product

---
+++ backend/src/main/kotlin/com/market/jumarket/model/Product.kt
@@ -0,0 +1,33 @@
+package com.market.jumarket.model
+
+import jakarta.annotation.Nullable
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.validation.constraints.NotBlank
+import java.math.BigDecimal
+
+@Entity
+data class Product(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val product_id: Long = 0,
+
+    @get: NotBlank
+    val name: String = "",
+
+    @get: NotBlank
+    val unit_of_measure: String = "",
+
+    @get: Nullable
+    val unit_price: BigDecimal = BigDecimal.ZERO,
+
+    @get: Nullable
+    val quantity: Long? = 0,
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    val category: Category? = null
+)
