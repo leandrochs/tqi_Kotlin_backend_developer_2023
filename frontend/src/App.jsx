c586979cd5b0bd2a1cffa3bada76c3e87cf0d6d2
leandrochs <leandrochagask@gmail.com>
2023-07-24 23:34:44 -0300

    feat: adiciona novas rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Provider from './context/Provider';
-import Home from './pages/Home';
-import Admin from './pages/Admin';
+import Admin from './pages/admin/Admin';
+import Home from './pages/home/Home';
+import GetCategory from './pages/getCategory/GetCategory';
+import PostCategory from './pages/postCategory/PostCategory';
 
 function App() {
   return (
     <Switch>
       <Provider>
         <Route exact path='/admin' component={Admin} />
+        <Route exact path='/admin/getcategory' component={GetCategory} />
+        <Route exact path='/admin/postcategory' component={PostCategory} />
         <Route exact path='/' component={Home} />
       </Provider>
     </Switch>
