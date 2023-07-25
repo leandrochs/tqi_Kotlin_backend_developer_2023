976c3bd216fbb2b0dcadc522fed7af71ab2db5f3
leandrochs <leandrochagask@gmail.com>
2023-07-24 23:31:55 -0300

    feat: adiciona opcoes para pagina de admin

---
+++ frontend/src/component/adminOptions/AdminOptions.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './adminOptions.css';
+
+export default function AdminOptions() {
+
+  return (
+    <div id="admin-container">
+      <Link to="/admin/getcategory" className='link-admin'>
+        Consultar Categorias
+      </Link>
+      <Link to="/admin/postcategory" className='link-admin'>
+        Cadastrar Categorias
+      </Link>
+      <Link to="/putcategory" className='link-admin'>
+        Atualizar Categorias
+      </Link>
+      <Link to="/getproduct" className='link-admin'>
+        Consultar Produtos
+      </Link>
+      <Link to="/postproduct" className='link-admin'>
+        Cadastrar Produtos
+      </Link>
+      <Link to="/putproduct" className='link-admin'>
+        atualizar Produtos
+      </Link>
+    </div>
+  );
+}
