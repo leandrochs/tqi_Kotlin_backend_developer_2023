a7cafeae555054a7cab2a3ce3f0137a7cfb60c43
leandrochs <leandrochagask@gmail.com>
2023-07-30 23:50:36 -0300

    feat: adiciona tela de mercado

--- frontend/src/pages/market/Market.jsx
+++ frontend/src/pages/market/Market.jsx
@@ -1,11 +1,19 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import './market.css';
+import GetCategoryForMarket from '../getCategoryForMarket/GetCategoryForMarket';
 
 export default function Market() {
 
   return (
     <div>
-MMM
+      <h1 id='title-container'>JUMARKET</h1>
+      <div id='link-cart-container'>
+        <Link to="/market/cart">
+          Ver Carrinho
+        </Link>
+      </div>
+        <GetCategoryForMarket />
     </div>
   );
 }
